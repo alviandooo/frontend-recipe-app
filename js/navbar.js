81ca9fde28a1477c0eebfd6dcc528d9b5c278dc6
Alviando <restualviando28@gmail.com>
2023-01-10 10:01:06 +0700

    [DELETE] file & folder old HTML

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", (event) => {
-  let scroll = this.scrollY;
-  let element = document.querySelector("#navbar");
-  if (scroll > 50) {
-    element.style.background = "#efc81a";
-  } else {
-    element.style.background = "";
-  }
-});
